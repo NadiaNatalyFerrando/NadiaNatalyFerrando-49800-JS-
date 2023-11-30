b1be52055956a3f31286e19086e6e3abaf66646e
Nadia Nataly Ferrando <nadianatalyferrando@gmail.com>
2023-11-29 23:55:53 -0300

    actualización por mal uncionamiento de funcion

--- Desktop/coder-proyecto/javaScript/js-49800-1er-entrega/JS-49800-NadiaFerrando/js/main.js
+++ Desktop/coder-proyecto/javaScript/js-49800-1er-entrega/JS-49800-NadiaFerrando/js/main.js
@@ -34,7 +34,7 @@ function requestSurname() {
 requestSurname();
 
 function requestBirthdate() {
-  let birthdate = prompt("Ingrese su fecha de nacimmiento");
+  let birthdate = parseInt (prompt("Ingrese su fecha de nacimmiento"));
   console.log(`La fecha de nacimiento es ${birthdate}`);
 }
 
@@ -71,39 +71,64 @@ requestEmail();
 const data = "A continuación, dejo expresa constancia que la siguiente información solicitada, es cierta y verdadera:";
   console.log(data); 
 
-//Definir las preguntas y respuestas
-const questions1 = "¿Padece afecciones cardiacas?";
-const questions2 = "¿Padece afecciones respiratorias?";
-const questions3 = "¿Padece afecciones neurológicas?";
-const questions4 = "¿Padece lesiones articulares?";
-const questions5 = "¿Padece lesiones óseas (fracturas)?";
-const questions6 = "¿Padece presión arterial normal?";
-const questions7 = "¿Fuma?";
+const answerCheck = (userAnswer, questions) => {
+  let answer = prompt(questions).toLowerCase();
 
-//Definir un objeto para almacenar las respuestas
-const answer = {};
-
-//Función para mostrar una pregunta y obtener la respuesta del usuario
-function askQuestions (questions) {
-  const answer = prompt(questions + " (responde si o no)").toLowerCase();
-  console.log (questions + ": " + (answer ? "Sí" : "No"));
-}
-
-answer[questions1] = askQuestions (questions1);
-answer[questions2] = askQuestions (questions2);
-answer[questions3] = askQuestions (questions3);
-answer[questions4] = askQuestions (questions4);
-answer[questions5] = askQuestions (questions5);
-answer[questions6] = askQuestions (questions6);
-answer[questions7] = askQuestions (questions7);
-
-function validateAnswer (answer){
-  if (answer === "sí" || answer === "si") {
-    console.log ("Respuesta: Sí");
-  } else if (answer === "no") {
-    console.log ("Respuesta: No");
-  } else {
-    return answer.toLowerCase() === "no";
+  while (answer !== "no" && answer !== "si") {
+    alert("Respuesta invalida. Por favor debe contestar por si o por no");
+    answer = prompt(questions).toLowerCase();
   }
+
+  if (answer === userAnswer) {
+    console.log(questions + ": " + answer)
+  } else {
+    console.log(questions + ": " + answer)
+  }
+};
+
+for (let i = 0; i <= 7; i++) {
+let questions;
+let userAnswer;
+
+switch (i) {
+  case 1:
+    questions = "¿Padece afecciones cardiacas?";
+    userAnswer = " ";
+    answerCheck (userAnswer, questions);
+    break;
+  case 2:
+    questions = "¿Padece afecciones respiratorias?";
+    userAnswer = " ";
+    answerCheck (userAnswer, questions);
+    break;
+  case 3:
+    questions = "¿Padece afecciones neurológicas?";
+    userAnswer = " ";
+    answerCheck (userAnswer, questions);
+    break;
+  case 4:
+    questions = "¿Padece lesiones articulares?";
+    userAnswer = " ";
+    answerCheck (userAnswer, questions);
+    break;
+  case 5:
+    questions = "¿Padece lesiones óseas (fracturas)?";
+    userAnswer = " ";
+    answerCheck (userAnswer, questions);
+    break;
+  case 6:
+    questions = "¿Padece presión arterial normal?";
+    userAnswer = " ";
+    answerCheck (userAnswer, questions);
+    break;
+  case 7:
+    questions = "¿Fuma?";
+    userAnswer = " ";
+    answerCheck (userAnswer, questions);
+    break;
+  default:
+    alert("Encuesta de preguntas por si o por no");
+    break;
+}
 }
 
